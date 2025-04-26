8cf427ac804170fbe1a552bd4a74bdbc849d07cd
rawcptr <rawcptr@icloud.com>
2025-04-26 10:40:28 +0530

    add flake.nix and update configuration.nix for flakes

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+	description = "NixOS System Configuration";
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+		home-manager.url = "github:nix-community/home-manager";
+		home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+		zen-browser.url = "github:youwen5/zen-browser-flake";
+		zen-browser.inputs.nixpkgs.follows = "nixpkgs";
+	};
+	
+	outputs = { self, nixpkgs, zen-browser, ... }@inputs:
+	let
+		system = "x86_64-linux"; 
+		pkgs = import nixpkgs {
+			inherit system;
+			config.allowUnfree = true;
+		};
+	in
+	{
+		nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
+			inherit system pkgs;
+			specialArgs = { inherit inputs; };
+			
+			modules = [
+				./configuration.nix
+				./hardware-configuration.nix
+			];
+		};
+	};
+}
+		
