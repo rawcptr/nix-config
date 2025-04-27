f3c4289ed7ea564ce72edeff0e61d3a802e3ead5
rawcptr <rawcptr@icloud.com>
2025-04-27 14:30:28 +0530

    remove dead file

--- home/shared/packages.nix
+++ home/shared/packages.nix
@@ -1,8 +1,24 @@
-{ pkgs, ... }:
+{ pkgs, inputs, ... }:
 
+let
+    zen-browser = inputs.zen-browser.packages.${pkgs.system}.default;
+in
 {
     home.packages = with pkgs; [
+	# terminals
+	ghostty
+
+	# cli programs
 	ripgrep
+	tree
+	fastfetch
+	wl-clipboard
 	
+	# essentials
+	zen-browser
+
+	# Hardware & Montoring
+	lm_sensors
+
     ];
 }
--- modules/shared/packages.nix
+++ modules/shared/packages.nix
@@ -10,26 +10,13 @@
     htop
     curl
     man-db # Provides man pages (man is often just a wrapper)
-    tree
-    ripgrep
     git
     nushell # Make sure it's available system-wide too
-    fastfetch
-    wl-clipboard # Wayland clipboard utilities
-    usbutils # lsusb
-    pciutils # lspci
-
-    # Hardware & Monitoring
-    lm_sensors
 
     # Build Tools (Consider if needed globally or better in devShells)
     gcc
     clang
     llvm
     uv # Python packaging
-
-    # Terminals / GUI Apps (consider if better in user env/HM)
-    ghostty
-    inputs.zen-browser.packages.${pkgs.system}.default # Zen Browser via flake input
   ];
 }
